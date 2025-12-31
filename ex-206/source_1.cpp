d5539c9f2d64d3e64e8fd8e3fa8b55d8ddd934c7
s100-y <lumberjackelder@gmail.com>
2025-12-31 11:01:52 +0700

    206_2

--- ex-206/source_1.cpp
+++ ex-206/source_1.cpp
@@ -10,32 +10,74 @@ content: Given the head of a singly linked list,
 //pre-processor
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include <stack>
+
 
 //definition
-std:: stack<int> s1;
-void convert_binary(unsigned int x);
-
-//main function
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode {
+      int val;
+      struct ListNode *next;
+ };
+struct ListNode* reverseList(struct ListNode* head);
+void reverseList_2(struct ListNode** head_ref);
+    
+//mainfunction
 int main(){
-    unsigned int x;
-    printf("nhap so muon quy doi sang he nhi phan:\nx=\n");scanf("%u",&x);
-    convert_binary(x);
+    // 1. Tạo thử node 1 (giá trị 1)
+    struct ListNode* n1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    n1->val = 1;
+    
+    // 2. Tạo thử node 2 (giá trị 2)
+    struct ListNode* n2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    n2->val = 2;
+    n1->next = n2;
+    n2->next = NULL;
+
+    printf("Original: 1 -> 2\n");
+
+    // 3. Gọi hàm xử lý
+    struct ListNode* result = reverseList(n1);
+
+    // 4. In kết quả
+    printf("Reversed: ");
+    while (result != NULL) {
+        printf("%d -> ", result->val);
+        result = result->next;
+    }
+    printf("NULL\n");
+
     return 0;
 }
 
 //sub-program
-void convert_binary(unsigned int x){
-    if(x==0)  printf("Binary= 00000000");
-    while (x>0){
-        int temp=x%2;
-        s1.push(temp);
-        x=x/2;
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+    while (curr != NULL) {
+        struct ListNode *nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
     }
-    printf("binary= ");
-    while(!s1.empty()){
-        printf("%d",s1.top());
-        s1.pop();
+    return prev;
+}
+
+void reverseList_2(struct ListNode** head_ref) {
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = *head_ref;
+    struct ListNode *next = NULL;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
+    *head_ref = prev; // Cập nhật lại giá trị của head gốc
+    return;
 }
